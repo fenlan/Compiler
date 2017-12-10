2ef57c9937bc73223d10ed34fd551c51e67c0fd7
fenlan <1475307818@qq.com>
2017-12-11 03:46:46 +0800

    语义分析

--- semantics.cpp
+++ semantics.cpp
@@ -1,10 +1,10 @@
 #include "semantics.h"
 
 extern double
-		Parameter,
-		Origin_x, Origin_y,
-		Scale_x, Scale_y,
-		Rot_angle;
+Parameter,
+Origin_x, Origin_y,
+Scale_x, Scale_y,
+Rot_angle;
 
 double GetExprValue(ExprNode *root);
 void DrawPixel(unsigned long x, unsigned long y);
@@ -14,7 +14,7 @@ void DelExprTree(ExprNode *root);
 static void Errmsg(char *string);
 static void CalcCoord(ExprNode *Hor_Exp, ExprNode *Ver_Exp, double &Hor_x, double &Ver_y);
 
-void Errmsg(char *string){ exit(1); }
+void Errmsg(char *string) { exit(1); }
 static void CalcCoord(ExprNode *Hor_Exp, ExprNode *Ver_Exp, double &Hor_x, double &Ver_y)
 {
 	double HorCord, VerCord, Hor_tmp;
@@ -35,7 +35,7 @@ void DrawLoop(double start, double end, double step, ExprNode *HorPtr, ExprNode 
 {
 	extern double Parameter;
 	double x, y;
-	for(Parameter = start; Parameter <= end; Parameter += step)
+	for (Parameter = start; Parameter <= end; Parameter += step)
 	{
 		CalcCoord(HorPtr, VerPtr, x, y);
 		DrawPixel((unsigned long)x, (unsigned long)y);
@@ -44,83 +44,56 @@ void DrawLoop(double start, double end, double step, ExprNode *HorPtr, ExprNode 
 
 double GetExprValue(ExprNode *root)
 {
-	if(root == NULL) 		return 0.0;
-	switch(root->node_type)
+	if (root == NULL) 		return 0.0;
+	switch (root->node_type)
 	{
-		case PLUS:
-			return GetExprValue(root->Content.OperatorNode.left) +
-					GetExprValue(root->Content.OperatorNode.right);
-		case MINUS:
-			return GetExprValue(root->Content.OperatorNode.left) -
-					GetExprValue(root->Content.OperatorNode.right);
-		case MUL:
-			return GetExprValue(root->Content.OperatorNode.left) *
-					GetExprValue(root->Content.OperatorNode.right);
-		case DIV:
-			return GetExprValue(root->Content.OperatorNode.left) /
-					GetExprValue(root->Content.OperatorNode.right);
-		case POWER:
-			return pow(GetExprValue(root->Content.OperatorNode.left),
-					GetExprValue(root->Content.OperatorNode.right));
-		case FUNC:
-			return (*root->Content.FuncNode.mathFuncPtr)
-					(GetExprValue(root->Content.FuncNode.child));
-		case CONST_ID:
-			return root->Content.ConstNode;
-		case T:
-			return *(root->Content.ParmPtrNode);
-		default :
-			return 0.0;
+	case PLUS:
+		return GetExprValue(root->Content.OperatorNode.left) +
+			GetExprValue(root->Content.OperatorNode.right);
+	case MINUS:
+		return GetExprValue(root->Content.OperatorNode.left) -
+			GetExprValue(root->Content.OperatorNode.right);
+	case MUL:
+		return GetExprValue(root->Content.OperatorNode.left) *
+			GetExprValue(root->Content.OperatorNode.right);
+	case DIV:
+		return GetExprValue(root->Content.OperatorNode.left) /
+			GetExprValue(root->Content.OperatorNode.right);
+	case POWER:
+		return pow(GetExprValue(root->Content.OperatorNode.left),
+			GetExprValue(root->Content.OperatorNode.right));
+	case FUNC:
+		return (*root->Content.FuncNode.mathFuncPtr)
+			(GetExprValue(root->Content.FuncNode.child));
+	case CONST_ID:
+		return root->Content.ConstNode;
+	case T:
+		return *(root->Content.ParmPtrNode);
+	default:
+		return 0.0;
 	}
 }
 
 void DelExprTree(ExprNode *root)
 {
-	if(root == NULL)	return;
-	switch(root->node_type)
+	if (root == NULL)	return;
+	switch (root->node_type)
 	{
-		case PLUS:	case MINUS:		case MUL:	case DIV:
-		case POWER:
-			DelExprTree(root->Content.OperatorNode.left);
-			DelExprTree(root->Content.OperatorNode.right);
-			break;
-		case FUNC:
-			DelExprTree(root->Content.FuncNode.child);
-			break;
-		default :
-			break;
+	case PLUS:	case MINUS:		case MUL:	case DIV:
+	case POWER:
+		DelExprTree(root->Content.OperatorNode.left);
+		DelExprTree(root->Content.OperatorNode.right);
+		break;
+	case FUNC:
+		DelExprTree(root->Content.FuncNode.child);
+		break;
+	default:
+		break;
 	}
 	delete(root);
 }
 
-#ifdef _BC_COMPILER
-	int InGraphMode = 0;
-	int InitGraph()
-	{
-		int gd = DETECT, gm;
-		if(InGraphMode)		return 0;
-		registerbgidriver(EGAVGA_driver);
-		initgraph(&gd, &gm, "");
-		setcolor(-1);
-		InGraphMode = 1;
-		return 1;
-	}
-	void CloseGraph()
-	{
-		if(!InGraphMode)	return;
-		getch();
-		closegraph();
-		InGraphMode = 0;
-	}
-#endif
-
 void DrawPixel(unsigned long x, unsigned long y)
 {
-	#ifdef _VC_COMPILER
-		SetPixel(hDC, x, y, black);
-	#endif
-
-	#ifdef _BC_COMPILER
-		putpixel(x, y, white);
-	#endif
+	SetPixel(hDC, x, y, black);
 }
--- semantics.h
+++ semantics.h
@@ -1,35 +1,16 @@
 #ifndef SEMANTICS_H
 #define SEMANTICS_H
 
-#ifdef _VC_COMPILER
-	#include <windows.h>
-	#include <wingdi.h>
-	extern HDC hDC;
-#endif
-#ifdef _BC_COMPILER
-	#include <graphics.h>
-	#include <conio.h>
-#endif
-
+#include <windows.h>
+#include <wingdi.h>
 #include "parser.h"
-#ifdef _VC_COMPILER
-	#define red RGB(255, 0, 0)
-	#define black RGB(0, 0, 0)
-#endif
+#define red RGB(255, 0, 0)
+#define black RGB(0, 0, 0)
 
-#ifdef _BC_COMPILER
-	#define white 255
-#endif
-
+extern HDC hDC;
 extern void DrawPixel(unsigned long x, unsigned long y);
 extern double GetExprValue(ExprNode *root);
 extern void DrawLoop(double start, double end, double step, ExprNode *HorPtr, ExprNode *VerPtr);
 extern void DelExprTree(ExprNode *root);
-
-#ifdef _BC_COMPILER
-	extern int InGraphMode;
-	extern int InitGraph();
-	extern void CloseGraph();
-#endif
 
 #endif
